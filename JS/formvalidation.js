36a8fca38191935aa4b62d198ab6e3f8dbf39b05
Bishal Singh <meh.bishal@gmail.com>
2018-08-09 07:46:42 +0545

    New js related files added

---
+++ JS/formvalidation.js
@@ -0,0 +1,7 @@
+function formValidation() {
+	var username = document.getElementById("uname").value;
+	var password = document.getElementById("pw").value;
+	
+	console.log(username);
+	document.write(password);
+}
